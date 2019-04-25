bb09c44f088041d01a83945b15c33f26c58fea7e
Telinc1 <telinc@telinc1.com>
2019-04-25 23:42:41 +0300

    Add the ability to select any local image

--- src/index.js
+++ src/index.js
@@ -5,9 +5,15 @@ document.addEventListener("DOMContentLoaded", function(){
     const tilemap = simulator.tilemap;
 
     const tilemapImage = document.querySelector("#tilemap-image > img");
+    const tilemapFile = document.getElementById("tilemap-file");
+    const tilemapFileAlert = document.getElementById("tilemap-file-alert");
 
     $("#tilemap-image-tab").on("show.bs.tab", (event) => {
         tilemapImage.src = tilemap.src;
+    });
+
+    $("#tilemap-upload-tab").on("show.bs.tab", (event) => {
+        tilemapFileAlert.classList.add("d-none");
     });
 
     Array.prototype.forEach.call(document.getElementById("tilemap-choices").children, (item) => {
@@ -20,6 +26,43 @@ document.addEventListener("DOMContentLoaded", function(){
         image.addEventListener("click", (event) => {
             tilemap.src = image.src;
         });
+    });
+
+    tilemapFile.addEventListener("change", (event) => {
+        const {files} = tilemapFile;
+
+        if(files.length === 0){
+            return;
+        }
+
+        const file = files[0];
+
+        if(!file.type.startsWith("image/")){
+            tilemapFileAlert.innerText = "You must select an image file.";
+
+            const {classList} = tilemapFileAlert;
+            classList.add("alert-danger");
+            classList.remove("alert-success");
+            classList.remove("d-none");
+
+            return;
+        }
+
+        tilemapFileAlert.classList.add("d-none");
+
+        const reader = new FileReader();
+        reader.onload = (event) => {
+            tilemap.src = event.target.result;
+
+            tilemapFileAlert.innerText = "Image selected.";
+
+            const {classList} = tilemapFileAlert;
+            classList.add("alert-success");
+            classList.remove("alert-danger");
+            classList.remove("d-none");
+        };
+
+        reader.readAsDataURL(file);
     });
 
     simulator.render();
